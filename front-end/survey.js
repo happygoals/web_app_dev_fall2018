1e3d6b466d243b5c150cb1a08aecfc462e7df326
N-Barn21 <7nbarnardsbs@gmail.com>
2018-10-15 22:11:54 +0000

    Added more javascript and am working on logic for submission checking

---
+++ front-end/survey.js
@@ -0,0 +1,30 @@
+function yesnoCheck() {
+    if (document.getElementById('yes1').checked || document.getElementById('no1').checked) {
+        document.getElementById('why-group1').style.visibility = 'visible';
+    }
+    else{
+        document.getElementById('why-group1').style.visibility = 'hidden';
+    }
+    if (document.getElementById('yes2').checked || document.getElementById('no2').checked) {
+        document.getElementById('why-group2').style.visibility = 'visible';
+    }
+    else{
+        document.getElementById('why-group2').style.visibility = 'hidden';
+    }
+
+}
+
+function validate()
+{
+	if (document.forms.survey.name.value == '')
+	{
+		alert("You must provide a user name!");
+		return false;
+	}
+	else if (!document.forms.survey.email.value.match(/.+@.+\.edu$/) || !document.forms.survey.email.value.match(/.+@.+\.com$/))
+	{
+		alert("You must provide a .edu or .com email adddress!");
+		return false;
+	}
+	return true;
+}
