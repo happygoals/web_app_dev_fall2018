e9134f41c0dcd070d27ba87511d96a1b182f1461
Mackenzie Crawford <brickmack@gmail.com>
2018-10-16 22:47:24 +0000

    added linechart, moved analytics page header outside switcher, reworking chart creation functionality

--- front-end/js/lineChart.js
+++ front-end/js/lineChart.js
@@ -1,46 +1,33 @@
-var ctx = document.getElementById("lineChart1").getContext('2d');
-var myChart = new Chart(ctx, {
-	type: 'line',
-	data: {
-		labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-		datasets: [{
-			label: '# of Votes',
-			data: [12, 19, 3, 5, 2, 3],
-			backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
-			borderColor: ['rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
-			borderWidth: 1
-		}]
-	},
-	options: {
-		scales: {
-			yAxes: [{
-				ticks: {
-					beginAtZero: true
-				}
+var data = [12, 19, 3, 5, 2, 3];
+var backgroundColor = ['rgba(255, 99, 132, 0.2)'];
+
+addChart("lineChart1", data, backgroundColor);
+
+var data2 = [10, 9, 9, 15, 20, 4];
+addChart("lineChart2", data2, backgroundColor);
+
+function addChart(targetID, inputData, backgroundColors) {
+	var ctx = document.getElementById(targetID).getContext('2d');
+	var myChart = new Chart(ctx, {
+		type: 'line',
+		data: {
+			labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+			datasets: [{
+				label: '# of Votes',
+				data: inputData,
+				backgroundColor: backgroundColors,
+				borderColor: ['rgba(255,99,132,1)'],
+				borderWidth: 1
 			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true
+					}
+				}]
+			}
 		}
-	}
-});
-var ctx2 = document.getElementById("lineChart2").getContext('2d');
-var myChart2 = new Chart(ctx2, {
-	type: 'line',
-	data: {
-		labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-		datasets: [{
-			label: '# of Votes',
-			data: [1, 9, 23, 7, 12, 3],
-			backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
-			borderColor: ['rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
-			borderWidth: 1
-		}]
-	},
-	options: {
-		scales: {
-			yAxes: [{
-				ticks: {
-					beginAtZero: true
-				}
-			}]
-		}
-	}
-});
+	});
+}
