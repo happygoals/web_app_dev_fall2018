7583f382f67fab92b8b0951ddff0f8cf27c2cc4f
Haemin Ryu <happygoals@naver.com>
2018-10-16 21:48:04 +0000

    formatting create pretty code for all html/css/js files

--- front-end/modal.js
+++ front-end/modal.js
@@ -1,26 +1,15 @@
-
-
 $(window, document, undefined).ready(function() {
-
-  $('.input').blur(function() {
-    var $this = $(this);
-    if ($this.val())
-      $this.addClass('used');
-    else
-      $this.removeClass('used');
-  });
-  
-  });
-
-
-$('#tab1').on('click' , function(){
-    $('#tab1').addClass('login-shadow');
-   $('#tab2').removeClass('signup-shadow');
+	$('.input').blur(function() {
+		var $this = $(this);
+		if ($this.val()) $this.addClass('used');
+		else $this.removeClass('used');
+	});
 });
-
-$('#tab2').on('click' , function(){
-    $('#tab2').addClass('signup-shadow');
-   $('#tab1').removeClass('login-shadow');
-
-
+$('#tab1').on('click', function() {
+	$('#tab1').addClass('login-shadow');
+	$('#tab2').removeClass('signup-shadow');
+});
+$('#tab2').on('click', function() {
+	$('#tab2').addClass('signup-shadow');
+	$('#tab1').removeClass('login-shadow');
 });
--- front-end/survey.js
+++ front-end/survey.js
@@ -1,27 +1,21 @@
 function expandCollapseBox(x, val) {
-    if (val == 1) {
-        document.getElementById(x).style.display = "block";
-    }
-    else {
-        document.getElementById(x).style.display = "none";
-    }
+	if (val == 1) {
+		document.getElementById(x).style.display = "block";
+	} else {
+		document.getElementById(x).style.display = "none";
+	}
 }
 
-function disable()
-{   
-    /* Allows the submit button to be either disabled or enabled */
-    document.getElementById("submit").disabled = !document.getElementById("submit").disabled;
+function disable() {
+	/* Allows the submit button to be either disabled or enabled */
+	document.getElementById("submit").disabled = !document.getElementById("submit").disabled;
 }
 
-function validate()
-{
-	if (document.forms.survey.name.value == '')
-	{
+function validate() {
+	if (document.forms.survey.name.value == '') {
 		alert("You must provide a user name!");
 		return false;
-	}
-	else if (!document.forms.survey.email.value.match(/.+@.+\.edu$/))
-	{
+	} else if (!document.forms.survey.email.value.match(/.+@.+\.edu$/)) {
 		alert("You must provide a .edu or .com email adddress!");
 		return false;
 	}
