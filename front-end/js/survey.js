f3d89936350048e2bc1ac309a0c92d70e549f8f4
N-Barn21 <7nbarnardsbs@gmail.com>
2018-10-17 05:39:44 +0000

    Finalized input testing, and added another further question

--- front-end/js/survey.js
+++ front-end/js/survey.js
@@ -1,14 +1,14 @@
-function expandCollapseBox(x, val) {
-	if (val == 1) {
+function expandCollapseBox(x, y) {
+	document.getElementById(x).style.display = "block";
+	document.getElementById(y).style.display = "none";
+}
+
+function expandCollapseBoxB(x) {
+	if (document.getElementById(x).style.display == "none") {
 		document.getElementById(x).style.display = "block";
 	} else {
 		document.getElementById(x).style.display = "none";
 	}
-}
-
-function expandCollapseBox(x, y) {
-	document.getElementById(x).style.display = "block";
-	document.getElementById(y).style.display = "none";
 }
 
 function disable() {
@@ -16,7 +16,7 @@ function disable() {
 	document.getElementById("submit").disabled = !document.getElementById("submit").disabled;
 }
 
-function isOneChecked(x) {
+function isOneCheckedRadio(x) {
   // All <input> tags...
   var form = x;
   var chx = x.getElementsByTagName('input');
@@ -32,39 +32,70 @@ function isOneChecked(x) {
   return false;
 }
 
+function isOneCheckedCheckbox(x) {
+  // All <input> tags...
+  var form = x;
+  var chx = x.getElementsByTagName('input');
+  for (var i=0; i<chx.length; i++) {
+    // If you have more than one radio group, also check the name attribute
+    // for the one you want as in && chx[i].name == 'choose'
+    // Return true from the function on first match of a checked item
+    if (chx[i].type == 'checkbox' && chx[i].checked) {
+      return true;
+    } 
+  }
+  // End of the loop, return false
+  return false;
+}
+
 function validate() {
 	// Checks all Question 1 inputs
-	if(isOneChecked(document.getElementById('question1')) == false){
+	if(isOneCheckedRadio(document.getElementById('question1')) == false){
 		alert("You must select an answer for question1!");
 		return false;
 	}
 	else {
 		if(document.getElementById('yes1').checked == true){
-			if(isOneChecked(document.getElementById('question1.2')) == false){
+			if(isOneCheckedRadio(document.getElementById('question1.2')) == false){
 				alert("You must select an answer for question 1's sub-question!");
 				return false;
 			}
 		}
 	}
 	// Checks all Question2 inputs
-	if(isOneChecked(document.getElementById('question2')) == false){
+	if(isOneCheckedRadio(document.getElementById('question2')) == false){
 		alert("You must select an answer for question2!");
 		return false;
 	}
 	else {
 		if(document.getElementById('yes2').checked == true){
-			if(isOneChecked(document.getElementById('question2.2')) == false){
+			if(isOneCheckedRadio(document.getElementById('question2.2')) == false){
 				alert("You must select an answer for question 2's sub-question!");
 				return false;
 			}
 		}
 	}
 	// Checks all Question3 inputs
-		if(isOneChecked(document.getElementById('question3')) == false){
+		if(isOneCheckedRadio(document.getElementById('question3')) == false){
 		alert("You must select an answer for question3!");
 		return false;
 	}
-	
+	//Check all Question4 inputs
+	if(isOneCheckedCheckbox(document.getElementById('question4')) == false){
+		alert("You must select an answer for question4!");
+		return false;
+	}
+	//Check all Question5 inputs
+	if(isOneCheckedRadio(document.getElementById('question5')) == false){
+		alert("You must select an answer for question5!");
+		return false;
+	}
+	//Check all Question6 inputs
+	if(isOneCheckedRadio(document.getElementById('question6')) == false){
+		alert("You must select an answer for question6!");
+		return false;
+	}
+	//Check Name and Email inputs
 	if (document.forms.survey.name.value == '') {
 		alert("You must provide a user name!");
 		return false;
