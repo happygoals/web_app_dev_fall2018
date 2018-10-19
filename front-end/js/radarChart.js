028f64b65498bd0f65763c3532f9f85ed66201a1
SporeRanier <mjwolgast@gmail.com>
2018-10-19 17:28:11 +0000

    adding radar chart

---
+++ front-end/js/radarChart.js
@@ -0,0 +1,30 @@
+var labels = ["Kitkat", "Starbucks coffee", "Cute cookie", "Sandwich", "Pizza"];
+var data = [12, 19, 3, 5, 2];
+var color = ['255, 99, 132', '54, 162, 235', '255, 206, 86', '75, 192, 192', '153, 102, 255'];
+
+addChart("radarChart1", labels, data, color);
+
+function addChart(targetID, inputLabels, inputData, backgroundColorsIn) {
+	var ctx = document.getElementById(targetID).getContext('2d');
+	var borderColors = [];
+	var backgroundColors = [];
+	
+	for (var i=0; i<backgroundColorsIn.length; i++) {
+		backgroundColors.push("rgba(" + backgroundColorsIn[i] + ", 0.2)");
+		borderColors.push("rgba(" + backgroundColorsIn[i] + ", 1.0)");
+	}
+
+	var pieChart = new Chart(ctx, {
+		type: "radar",
+		data: {
+			labels: inputLabels,
+			datasets: [{
+				label: '# of Votes',
+				data: inputData,
+				backgroundColor: backgroundColors,
+				borderColor: borderColors,
+				borderWidth: 1
+			}]
+		}
+	});
+}
