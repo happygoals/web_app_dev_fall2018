d71a77a7bf60081b6087a41fd220e6b1e5518290
Mack Crawford <crawmc02@pfw.edu>
2018-10-28 18:14:58 +0000

    fixed missing JS on survey, fixed text error on all 3 php pages

--- front-end/survey.php
+++ front-end/survey.php
@@ -10,7 +10,8 @@
 	<link href="css/signin.css" rel="stylesheet">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script><!-- Popper JS (what are we using this for?) -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script><!-- Popper JS -->
+	<script src="js/survey.js"></script>
 	<script src="js/modal.js"></script>
 	<script src="bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
 	<script src="Chart.js/Chart.min.js"></script>
