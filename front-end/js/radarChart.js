df3a8d1eb241ca2a833f223e0e6bac2c16a9f6c0
Mack Crawford <crawmc02@pfw.edu>
2018-11-02 22:02:07 +0000

    simplified radarChart.js since radar supports only a single color

--- front-end/js/radarChart.js
+++ front-end/js/radarChart.js
@@ -1,18 +1,13 @@
 var labels = ["Kitkat", "Starbucks coffee", "Cute cookie", "Sandwich", "Pizza"];
 var data = [12, 19, 3, 5, 2];
-var color = ['255, 99, 132'];
+var color = '255, 99, 132';
 
 addChart("radarChart1", labels, data, color);
 
 function addChart(targetID, inputLabels, inputData, backgroundColorsIn) {
 	var ctx = document.getElementById(targetID).getContext('2d');
-	var borderColors = [];
-	var backgroundColors = [];
-	
-	for (var i=0; i<backgroundColorsIn.length; i++) {
-		backgroundColors.push("rgba(" + backgroundColorsIn[i] + ", 0.2)");
-		borderColors.push("rgba(" + backgroundColorsIn[i] + ", 1.0)");
-	}
+	var borderColors = "rgba(" + backgroundColorsIn + ", 1.0)";
+	var backgroundColors = "rgba(" + backgroundColorsIn + ", 0.2)";
 
 	var radarChart = new Chart(ctx, {
 		type: "radar",
