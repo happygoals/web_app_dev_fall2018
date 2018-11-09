49dfcd61b05f43f9c87fe1b52834e5102f0a4cab
Mack Crawford <crawmc02@pfw.edu>
2018-11-09 18:30:33 +0000

    added redirect (to home for now, will look at redirecting to the same page later) on logout, minor cleanup

--- front-end/logout.php
+++ front-end/logout.php
@@ -1,32 +1,16 @@
 <?php 
-    /**
-     * logout.php
-     *
-     * A simple logout module for all of our login modules.
-     *
-     * Refer to CS75 (David J. Malan)
-     */
+  //enable sessions
+  session_start();
 
-    // enable sessions
-    session_start();
+  //delete cookies
+  setcookie("user", "", time() - 3600);
+  setcookie("pass", "", time() - 3600);
 
-    // delete cookies, if any
-    setcookie("user", "", time() - 3600);
-    setcookie("pass", "", time() - 3600);
-
-    // log user out
-    setcookie(session_name(), "", time() - 3600);
-    session_destroy();
+  //log user out
+  setcookie(session_name(), "", time() - 3600);
+  session_destroy();
+    
+  //redirect to home
+  header("Location: home.php");
+  exit()
 ?>
-
-<!DOCTYPE html>
-
-<html>
-  <head>
-    <title>Log Out</title>
-  </head>
-  <body>
-    <h1>You are logged out!</h1>
-    <h3><a href="home.php">home</a></h3>
-  </body>
-</html>
