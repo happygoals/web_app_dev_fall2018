8852cb0e71bf211f5f05deb1a1d55fb2a6e6d996
Mack Crawford <crawmc02@pfw.edu>
2018-11-09 18:33:56 +0000

    comment clarification and code styling

--- front-end/navbar/loginModal.php
+++ front-end/navbar/loginModal.php
@@ -1,37 +1,25 @@
 <?php
-    /**
-     * login4.php
-     *
-     * A simple login module that lets a user stay logged in
-     * by saving username and, ack, password in cookies.
-     *
-     * Refer to CS75 (David J. Malan)
-     */
-
-    // enable sessions
+    //enable sessions
     session_start();
 
-    // were this not a demo, these would be in some database
+    //should get these from a database later. Same username and password as the in-class demos for now
     define("USER", "cs372");
     define("PASS", "pfw");
     
-    if (isset($_POST["user"]) && isset($_POST["pass"]))
-    {
-        // if username and password are valid, log user in
-        if ($_POST["user"] == USER && $_POST["pass"] == PASS)
-        {
+    if (isset($_POST["user"]) && isset($_POST["pass"])) {
+        //if username and password are valid, log user in
+        if ($_POST["user"] == USER && $_POST["pass"] == PASS) {
             // remember that user's logged in
             $_SESSION["authenticated"] = true;
 
             // save username in cookie for a week
             setcookie("user", $_POST["user"], time() + 7 * 24 * 60 * 60);
 
-            // save password in, ack, cookie for a week if requested
-            if ($_POST["keep"])
+            //save password in cookie for a week if requested
+            if ($_POST["keep"]) {
                 setcookie("pass", $_POST["pass"], time() + 7 * 24 * 60 * 60);
+            }
 
-            // redirect user to home page, using absolute path, per
-            // http://us2.php.net/manual/en/function.header.php
             $host = $_SERVER['HTTP_HOST'];
             $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
             header("Location: http://$host$path/home.php");
