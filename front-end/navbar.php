9c027905966ce06c12d1eeebcd26c84d8531b614
Mack Crawford <crawmc02@pfw.edu>
2018-11-10 19:03:55 +0000

    fixed issue with text display next to icon in account dropdown, testing simplification of dropdown logic

--- front-end/navbar.php
+++ front-end/navbar.php
@@ -28,7 +28,18 @@
 		include 'navbar/loggedInDropdown.php';
 	}
 	else if ($_SESSION["authenticated"] == false) { //logged out
-	    include 'navbar/loggedOutDropdown.php';
+	    ?>
+	    <ul class="navbar-nav ml-auto ml-md-0">
+			<li class="nav-item dropdown no-arrow">
+				<a aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" id="userDropdown" role="button"><i class="fas fa-user-circle fa-fw"></i></a>
+				<div aria-labelledby="userDropdown" class="dropdown-menu dropdown-menu-right">
+					<a class="dropdown-item" data-target=".log-signin" data-toggle="modal" href="#signin">Sign in</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" data-target=".log-signup" data-toggle="modal" href="#signup">Sign up</a>
+				</div>
+			</li>
+		</ul>
+	    <?php
 	}
 ?>
 	</nav>
