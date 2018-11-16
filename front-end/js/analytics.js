c979780c51da2814561c96522b4ef20a89558718
Haemin Ryu <happygoals@naver.com>
2018-11-16 17:41:19 +0000

    remove the duplicated codes in analytics charts and boxes

---
+++ front-end/js/analytics.js
@@ -0,0 +1,9 @@
+function analyticsChange(x) {
+	if (document.getElementById(x).style.display == "none") {
+		document.getElementById(x).style.display = "block";
+	} else {
+		document.getElementById(x).style.display = "none";
+	}
+}
+
+
