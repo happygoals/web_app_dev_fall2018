5948a544b35c4216d5cbf77a0bcf1b48c40cdcba
Mack Crawford <crawmc02@pfw.edu>
2018-11-16 16:04:36 +0000

    code styling and cleanup in signup modal

--- front-end/navbar/signupModal.php
+++ front-end/navbar/signupModal.php
@@ -1,42 +1,40 @@
-<div class="modal fade bs-modal-sm log-signup" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+<div class="modal fade bs-modal-sm log-signup" id="signupModal" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
 	<div class="modal-dialog modal-sm">
 		<div class="modal-content">
 			<div class="modal-body">
-				<div class="tab-content" id="myTabContent">
-					<form class="form-horizontal">
-						<fieldset>
-							<div class="group">
-								<h4><b>Sign up</b></h4>
+				<form class="form-horizontal">
+					<fieldset>
+						<div class="group">
+							<h4><b>Sign up</b></h4>
+						</div>
+						<div class="group">
+							<!--first name-->
+							<input class="input" required="" type="text" id="firstName" name="firstName"><span class="highlight"></span><span class="bar"></span> <label class="label" for="firstName">First Name</label>
+						</div>
+						<div class="group">
+							<!--last name-->
+							<input class="input" required="" type="text" id="lastName" name="lastName"><span class="highlight"></span><span class="bar"></span> <label class="label" for="lastName">Last Name</label>
+						</div>
+						<div class="group">
+							<!--email-->
+							<input class="input" required="" type="text" id="email" name="email"><span class="highlight"></span><span class="bar"></span> <label class="label" for="email">Email</label>
+						</div>
+						<div class="group">
+							<!--username-->
+							<input class="input" required="" type="text" id="username" name="username"><span class="highlight"></span><span class="bar"></span> <label class="label" for="username">Username</label>
+						</div>
+						<div class="group">
+							<!--password-->
+							<input class="input" required="" type="password" id="password" name="password"><span class="highlight"></span><span class="bar"></span> <label class="label" for="password">Password</label>
+						</div><em>Minimum 8 Characters</em>
+						<div class="control-group">
+							<label class="control-label" for="confirmSignup"></label>
+							<div class="controls">
+								<button class="btn btn-warning btn-block" id="confirmSignup" name="confirmSignup">Sign Up</button>
 							</div>
-							<div class="group">
-								<!--first name-->
-								<input class="input" required="" type="text" id="firstName" name="firstName"><span class="highlight"></span><span class="bar"></span> <label class="label" for="firstName">First Name</label>
-							</div>
-							<div class="group">
-								<!--last name-->
-								<input class="input" required="" type="text" id="lastName" name="lastName"><span class="highlight"></span><span class="bar"></span> <label class="label" for="lastName">Last Name</label>
-							</div>
-							<div class="group">
-								<!--email-->
-								<input class="input" required="" type="text" id="email" name="email"><span class="highlight"></span><span class="bar"></span> <label class="label" for="email">Email</label>
-							</div>
-							<div class="group">
-								<!--username-->
-								<input class="input" required="" type="text" id="username" name="username"><span class="highlight"></span><span class="bar"></span> <label class="label" for="username">Username</label>
-							</div>
-							<div class="group">
-								<!--password-->
-								<input class="input" required="" type="password" id="password" name="password"><span class="highlight"></span><span class="bar"></span> <label class="label" for="password">Password</label>
-							</div><em>Minimum 8 Characters</em>
-							<div class="control-group">
-								<label class="control-label" for="confirmsignup"></label>
-								<div class="controls">
-									<button class="btn btn-warning btn-block" id="confirmsignup" name="confirmsignup">Sign Up</button>
-								</div>
-							</div>
-						</fieldset>
-					</form>
-				</div>
+						</div>
+					</fieldset>
+				</form>
 			</div>
 		</div>
 	</div>
