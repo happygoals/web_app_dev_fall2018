fe46f7231455b3d774215e22debc6e12cea2da9b
Mack Crawford <crawmc02@pfw.edu>
2018-11-22 06:03:19 +0000

    significant rework of analytics. Moved listbox, productrow, and headerboxes all into new common.php file, removed a bunch of redundant CSS, fixed table formatting issue for logged-out users

--- front-end/analytics.php
+++ front-end/analytics.php
@@ -12,7 +12,8 @@ session_start();
 <body>
 <?php
     include 'navbar.php';
-    headerFunction("navbar-dark bg-dark", "", __FILE__); //for everything else
+    include "common.php";
+    headerFunction("navbar-dark bg-dark", "", __FILE__);
 
     //determine if user has admin privs or not
     $adminPriv = false;
@@ -65,115 +66,75 @@ session_start();
 			</div>
 			<div class="col-sm-8 text-left">
 				<div class="row justify-content-around">
-					<div class="card text-white bg-warning mb-5" style="max-width: 18rem; margin:0px 0px 0px 25px;">
-						<div class="card-header" style="background-color:Orange;"><h6>&nbsp;&nbsp;&nbsp;&nbsp;Total Surveys&nbsp;&nbsp;&nbsp; &nbsp;</h6></div>
-							<div class="card-body" style="background-color:Orange;" >
-						    	<h3 style="color:white;  text-align:center;"><i class="far fa-clipboard" style="color:white;"></i> 257</h3>
-							</div>
-						</div>
-						<div class="card text-white bg-light mb-5" style="max-width: 18rem;  ">
-							<div class="card-header" style="background-color:Salmon;" ><h6></i>&nbsp;&nbsp;&nbsp;&nbsp;New Visitors &nbsp;&nbsp;&nbsp;&nbsp;</h6></div>
-								<div class="card-body" style="background-color:Salmon;" >
-						  			<h3 style="color:white;  text-align:center;"><i class="far fa-user" style="color:white;"></i> 19</h3>
-						  		</div>
-							</div>
-							<div class="card text-white bg-light mb-5" style="max-width: 20rem; ">
-								<div class="card-header" style="background-color:YellowGreen;" >
-									<h6>&nbsp;&nbsp;&nbsp;&nbsp;Popular Snack&nbsp;&nbsp;&nbsp;&nbsp;</h6>
-						  		</div>
-						  		<div class="card-body" style="background-color:YellowGreen;text-align:center;" >
-							  		<h3 style="color:white;  text-align:center;"><i class="fas fa-cookie-bite" style="color:white;"></i> KitKat</h3>
-								    <h4 class="card-title" style="background-color:YellowGreen; text-align:center; "></h4>
-						  		</div>
-							</div>
-						<div class="card text-white bg-light mb-5 " style="max-width: 18rem; margin: 0px 25px 0px 0px;">
-							<div class="card-header" style="background-color:OrangeRed;"><h6>&nbsp;Today's Hot Item&nbsp;</h6></div>
-								<div class="card-body" style="background-color:OrangeRed;  text-align:center;">
-									<h3 style="color:white;  text-align:center;"><i class="fab fa-hotjar" style="color:white;"></i> Coke</h3>
-							  	</div>
-							</div>
-						</div>
-						<!-- Bar -->
-						<div class="vis" id="bar" style="margin: 0px 350px;"> 
-							<!--vis does nothing here, just differentiates it from invisible for code readability -->
-							<div class="chartBox">
-								<canvas width="300" height="400" id="barGraph1"></canvas>
-							</div>
-							<script src="js/barGraph.js"></script>
-						</div>
-						<!-- Pie -->			
-						<div class="inv" id="pie" style="margin: 0px 350px;">
-							<div class="chartBox">
-								<canvas width="300" height="400" id="pieChart1"></canvas>
-							</div>
-							<script src="js/pieChart.js"></script>
-						</div>
-						<!-- Line -->				
-						<div class="inv" id="line" style="margin: 0px 350px;">
-							<div class="chartBox">
-								<canvas width="300" height="400" id="lineChart1" ></canvas>
-							</div>
-							<script src="js/lineGraph.js"></script>
-						</div>
-						<!-- Radar --> 
-						<div class="inv" id="radar" style="margin: 0px 350px;">
-							<div class="chartBox">
-								<canvas width="400" height="500" id="radarChart1"></canvas>
-							</div>
-							<script src="js/radarChart.js"></script>
-						</div>
-						<!-- Scatter-->
-						<div class="inv" id="scatter" style="margin: 0px 350px;">
-							<div class="chartBox">
-								<canvas width="300" height="400" id="scatterPlot1" ></canvas>
-							</div>
-							<script src="js/scatterPlot.js"></script>
-						</div>
-						<!--list boxes-->
-						<div class="row">
-							<div class="card bg-info text-white text-center p-4" style="margin: 20px 500px 20px 100px;">
-			    				<blockquote class="blockquote mb-0">
-			      					<h5 style="text-align:center;">Top Sale List</h5>
-									<ol style="text-align:left;">
-										<li>Coke</li>
-										<li>Orange Juice</li>
-										<li>Potato chips</li>
-									</ol>
-								</blockquote>
-							</div>
-						<div class="card bg-Secondary text-white text-center p-4" style="margin: 20px 100px 20px 123px;">
-			    				<blockquote class="blockquote mb-0">
-									<h5 style="text-align:center;">New Entry Lank</h5>
-									<ul style="text-align:left;">
-										<li>Cute Cookie</li>
-										<li>Buritto</li>
-										<li>Banana</li>
-									</ul>
-							  	</blockquote>
-							</div>
-						</div>
-						<!-- Table --> 	
-						<div>
-							<table class="table" id="table" style="-ms-overflow-style: -ms-autohiding-scrollbar; max-height: 200px;">
-								<thead>
-									<tr style="text-align:center;">
-										<th scope="col">#</th>
-										<th scope="col">Product Name</th>
-										<th scope="col">Location</th>
-										<th scope="col">Price</th>
-										<th scope="col">Action</th>
-									</tr>
-								</thead>
-								<tbody>
-							<?php
-								require 'productRow.php';
-								
-								productRow(1, "Kitkat", 1, 1.00, $adminPriv);
-								productRow(2, "Starbucks coffee", 2, 2.50, $adminPriv);
-								productRow(3, "Cute cookie", 1, 2.00, $adminPriv);
-								productRow(4, "Sandwich", 3, 2.30, $adminPriv);
-								productRow(5, "Pizza", 3, 3.00, $adminPriv);
-							?>
+					<?php
+						simpleBox("Orange", "far fa-clipboard", "Total Surveys", "257");
+						simpleBox("Salmon", "far fa-user", "New Visitors", "19");
+						simpleBox("YellowGreen", "fas fa-cookie-bite", "Popular Snack", "KitKat");
+						simpleBox("OrangeRed", "fab fa-hotjar", "Today's Hot Item", "Coke")
+					?>
+				</div>
+				<!-- Bar -->
+				<div class="vis" id="bar" style="margin: 0px 350px;"> 
+					<div class="chartBox">
+						<canvas width="300" height="400" id="barGraph1"></canvas>
+					</div>
+					<script src="js/barGraph.js"></script>
+				</div>
+				<!-- Pie -->			
+				<div class="inv" id="pie" style="margin: 0px 350px;">
+					<div class="chartBox">
+						<canvas width="300" height="400" id="pieChart1"></canvas>
+					</div>
+					<script src="js/pieChart.js"></script>
+				</div>
+				<!-- Line -->				
+				<div class="inv" id="line" style="margin: 0px 350px;">
+					<div class="chartBox">
+						<canvas width="300" height="400" id="lineChart1" ></canvas>
+					</div>
+					<script src="js/lineGraph.js"></script>
+				</div>
+				<!-- Radar --> 
+				<div class="inv" id="radar" style="margin: 0px 350px;">
+					<div class="chartBox">
+						<canvas width="400" height="500" id="radarChart1"></canvas>
+					</div>
+					<script src="js/radarChart.js"></script>
+				</div>
+				<!-- Scatter-->
+				<div class="inv" id="scatter" style="margin: 0px 350px;">
+					<div class="chartBox">
+						<canvas width="300" height="400" id="scatterPlot1" ></canvas>
+					</div>
+					<script src="js/scatterPlot.js"></script>
+				</div>
+				<!--list boxes-->
+				<div class="row">
+					<?php
+						listbox("#17a2b8", "Top Sale List", array("Coke", "Orange Juice", "Potato Chips"));
+						listbox("#6c757d", "New Entry Lank", array("Cute Cookie", "Buritto", "Banana"));
+					?>
+				</div>
+				<!-- Table --> 	
+				<div>
+					<table class="table" id="table" style="-ms-overflow-style: -ms-autohiding-scrollbar; max-height: 200px;">
+						<thead>
+							<tr style="text-align:center;">
+								<th scope="col">#</th>
+								<th scope="col">Product Name</th>
+								<th scope="col">Location</th>
+								<th scope="col">Price</th>
+								<th scope="col">Action</th>
+							</tr>
+						</thead>
+						<tbody>
+					<?php
+						productRow(1, "Kitkat", 1, 1.00, $adminPriv);
+						productRow(2, "Starbucks coffee", 2, 2.50, $adminPriv);
+						productRow(3, "Cute cookie", 1, 2.00, $adminPriv);
+						productRow(4, "Sandwich", 3, 2.30, $adminPriv);
+						productRow(5, "Pizza", 3, 3.00, $adminPriv);
+					?>
 						</tbody>
 					</table>
 				</div>
---
+++ front-end/common.php
@@ -0,0 +1,47 @@
+<?php
+function listbox($background, $title, $list) { ?>
+	<div class="card text-white text-center p-4" style="background-color: <?php echo $background ?>">
+		<blockquote class="blockquote mb-0">
+			<h5><?php echo $title ?></h5>
+			<ol style="text-align:left;">
+				<?php
+					//generate list from array
+					foreach ($list as $listItem) {
+						echo "<li>".$listItem."</li>\n";
+					}
+				?>
+			</ol>
+		</blockquote>
+	</div>
+<?php }
+?>
+
+<?php
+function simpleBox($background, $icon, $title, $value) { ?>
+	<div class="card text-white bg-light text-center mb-5" style="min-width: 11rem; max-width: 20rem;">
+		<div class="card-header" style="background-color:<?php echo $background ?>;">
+			<h6><?php echo $title ?></h6>
+		</div>
+		<div class="card-body" style="background-color:<?php echo $background ?>;">
+			<h3><i class="<?php echo $icon ?>"></i> <?php echo $value ?></h3>
+		</div>
+	</div>
+<?php } ?>
+
+<?php
+function productRow($rowNum, $name, $vmNum, $price, $adminPriv) { ?>
+    <tr>
+	    <th scope="row"><?php echo $rowNum;?></th>
+		<td><?php echo $name;?></td>
+		<td>Vending machine <?php echo $vmNum;?></td>
+		<td><?php echo money_format('$%i', $price);?></td>
+		<td style="text-align:center;">
+			<?php if ($adminPriv == true) { ?>
+		    	<button type="button" class="btn btn-outline-primary" value='dddRow'><i class="fas fa-plus"></i>&nbsp;Add</button> 
+			    <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash"></i>&nbsp;Delete</button>
+			<?php } ?>
+		</td>
+	</tr>
+<?php   
+}
+?>
