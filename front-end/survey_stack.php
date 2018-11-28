7d8d554ad75d594190e4af783f71471d019610bb
Min Namgung <ccindy9966@gmail.com>
2018-11-28 22:00:43 +0000

    DB input connection-need testing

--- front-end/survey_stack.php
+++ front-end/survey_stack.php
@@ -2,29 +2,31 @@
     
     require_once('connection.php');
     
-    $test1 = mysqli_real_escape_string($_REQUEST['opt']);
-    
-    
     if(isset($_POST['submit']))
     {
         $question1 = $_POST['opt'];         //question 1 from survey 1
         $question2 = $_POST['opt2'];        //gender
-        $question3 = $_POST['opt3'];
-        $question3_1 = $_POST['why']; 
-        $question3_2 = $_POST['op3.2.0'];
-        $question4 = $_POST['opt4'];
-        $question4_1 = $_POST['why2']; 
-        $question4_2 = $_POST['op4.2.0'];
-        $question5 = $_POST['opt5'];
+        $question3 = $_POST['opt3'];        //Purchasee_snack
+        $question3_1 = $_POST['why'];       //Purchasee_snack _ No_ not
+        $question3_2 = $_POST['op3.2.0'];   //Purchasee_snack _ Yes_ how often 
+        $question4 = $_POST['opt4'];        //Purchasee_Drink
+        $question4_1 = $_POST['why2'];      //Purchasee_drink _ No_ not
+        $question4_2 = $_POST['op4.2.0'];   //Purchasee_drink _ Yes_ how often 
+        $question5 = $_POST['opt5'];        //Prefer_price
         $question6 = $_POST['opt6'];
         $question7 = $_POST['opt7'];
         $question8 = $_POST['opt8'];
+        
+        //pop up choose one 
         $question9 = $_POST['op9.0.0'];
-        $question9_1 = $_POST['Why3'];
+        $question9_1 = $_POST['Why3'];      //other type of snacks 
+        
         $question10 = $_POST['op10.0.0'];
         $question10_1 = $_POST['Why4'];
+        
         $question11 = $_POST['op11'];
         $question11_1 = $_POST['Why5'];
+        
         $question12 = $_POST['op12'];
         $question12_1 = $_POST['Why6'];
         
@@ -40,22 +42,60 @@
         $question15_1 = $_POST['Why7'];
         $question16 = $_POST['Why8'];
         
-        $input_name = $_POST['name']; 
+        $name = $_POST['name']; 
         $email = $_POST['email']; 
         
         
-        $sqlQuery="INSERT INTO `survey`
-        (`Snack_ID`, 
-        `question1`, `Gender`, `Location`, `Purchase_Snack`, `Purchase_SYes`, `Purchase_SNo`, 
-        `Purchase_Drink`, `Purchase_DYes`, `Purchase_DNo`, `Prefer_price`, `Purchase_resaon`, `Payment`, 
-        `snack1`, `snack2`, `snack3`, `drink1`, `drink2`, `drink3`, `rmv_snack`, `rmv_drink`, `most_vd_building`, `most_vd`, `least_vd_building`, `least_vd`,
-        `helpful`, `helpful_no`, `extra_opinion`, `name`, `email`) VALUES
-        ([value-1],
-        '$question1','$question2','$question3','$question3_1','$question3_2',[value-7],
-        [value-8],[value-9],[value-10],[value-11],[value-12],[value-13],
-        [value-14],[value-15],[value-16],[value-17],[value-18],[value-19],[value-20],[value-21],[value-22],[value-23],[value-24],[value-25],[value-26],[value-27],[value-28],[value-29],[value-30])";
-        
-            
-    }
+        $sql = sprintf("INSERT INTO `survey1`(`question1`, `question2`, `question3`, `question3_1`, `question3_2`, 
+        `question4`, `question4_1`, `question4_2`, `question5`, `question6`, `question7`, `question8`, 
+        `question9`, `question9_1`, `question10`, `question10_1`, `question11`, `question11_1`, 
+        `question12`, `question12_1`, `question13`, `question13_1`, `question14`, `question14_1`,
+        `question15`, `question15_1`, `question16`, `name`, `email`) VALUES
+        ('$question1','$question2','$question3','$question3_1','$question3_2',
+        '$question4','$question4_1','$question4_2','$question5','$question6','$question7','$question8',
+        '$question9','$question9_1','$question10','$question10_1','$question11','$question11_1',
+        '$question12','$question12_1','$question13','$question13_1','$question14','$question14_1',
+        '$question15','$question15_1','$question16','$name','$email');",
+               
+                $connection->real_escape_string($_POST['opt']),
+                $connection->real_escape_string($_POST['opt2']),
+                $connection->real_escape_string($_POST['opt3']),
+                $connection->real_escape_string($_POST['why']),
+                $connection->real_escape_string($_POST['op3.2.0']),
+                $connection->real_escape_string($_POST['opt4']),
+                $connection->real_escape_string($_POST['why2']),
+                $connection->real_escape_string($_POST['op4.2.0']),
+                $connection->real_escape_string($_POST['opt5']),
+                $connection->real_escape_string($_POST['opt6']),
+                $connection->real_escape_string($_POST['opt7']),
+                $connection->real_escape_string($_POST['opt8']),
+                $connection->real_escape_string($_POST['op9.0.0']),
+                $connection->real_escape_string($_POST['Why3']),
+                $connection->real_escape_string($_POST['op10.0.0']),
+                $connection->real_escape_string($_POST['Why4']),
+                $connection->real_escape_string($_POST['op11']),
+                $connection->real_escape_string($_POST['Why5']),
+                $connection->real_escape_string($_POST['op12']),
+                $connection->real_escape_string($_POST['Why6']),
+                $connection->real_escape_string($_POST['']),   //question 13
+                $connection->real_escape_string($_POST['']),   //question 13_1
+                $connection->real_escape_string($_POST['']),   //question 14
+                $connection->real_escape_string($_POST['']),   //question 14_1
+                $connection->real_escape_string($_POST['opt15']),
+                $connection->real_escape_string($_POST['Why7']),
+                $connection->real_escape_string($_POST['Why8']),
+                $connection->real_escape_string($_POST['name']),
+                $connection->real_escape_string($_POST['email'])
+                );
+                
+                // execute query
+                $result = $connection->query($sql) or die(mysqli_error($connection));  
+    
+                if ($result === false)
+                    die("Could not query database");
+                else
+                    echo "User was inserted into DB!\n";
+                
+    } 
 
 ?>
