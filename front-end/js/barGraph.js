058b0ae510af0b07091f72e9ece46fd3bd0ed601
Haemin Ryu <happygoals@naver.com>
2018-12-01 00:57:37 +0000

    Connected the barGraph with DB Product

--- front-end/js/barGraph.js
+++ front-end/js/barGraph.js
@@ -1,34 +1,42 @@
+var color = ['255, 99, 132', '54, 162, 235', '255, 206, 86', '75, 192, 192', '153, 102, 255'];
+
 $(document).ready(function(){
 	$.ajax({
-			
-		url: "../chartdata.php",
+		url: "/front-end/analytics/data.php",
 		method: "GET",
 		success: function(data) {
 			console.log(data);
-			var items = [];
-			var numbers = [];
-
+			var item = [];
+			var would = [];
+			
+			//
+        	var borderColors = [];
+        	var backgroundColors = [];
+        	
+        	for (var i=0; i<color.length; i++) {
+        		backgroundColors.push("rgba(" + color[i] + ", 0.2)");
+        		borderColors.push("rgba(" +color[i] + ", 1.0)");
+        	}
+            //
+            
+            
 			for(var i in data) {
-				items.push(data[i].name);
-				numbers.push(data[i].wouldPurchase);
+				item.push(data[i].name);
+				would.push(data[i].wouldPurchase);
 			}
-	
-
-		var chartdata= {
-				labels: items,
-				datasets: [{
-								label: '# of Votes',
-								backgroundColor: 'rgba(200, 200, 200, 0.75)',
-								borderColor: 'rgba(200, 200, 200, 0.75)',
-								hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
-								hoverBorderColor: 'rgba(200, 200, 200, 1)',
-		        				borderWidth: 1,
-								data: numbers
-							}
-						]
+			var chartdata = {
+				labels: item,
+				datasets : [
+					{
+						label: '# of Votes',
+        				backgroundColor: backgroundColors,
+        				borderColor: borderColors,
+        				borderWidth: 1,
+						data: would
+					}
+				]
 			};
-			
-			
+
 			var ctx = $("#barGraph1");
 
 			var barGraph = new Chart(ctx, {
