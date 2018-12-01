950b81f3d4f50df8021609dcab6f8c0ceb3f680d
Haemin Ryu <happygoals@naver.com>
2018-12-01 01:50:41 +0000

    pie chart start

--- front-end/js/pieChart.js
+++ front-end/js/pieChart.js
@@ -1,30 +1,51 @@
-var labels = ["Kitkat", "Starbucks coffee", "Cute cookie", "Sandwich", "Pizza"];
-var data = [12, 19, 3, 5, 2];
 var color = ['255, 99, 132', '54, 162, 235', '255, 206, 86', '75, 192, 192', '153, 102, 255'];
 
-addPieChart("pieChart1", labels, data, color);
+$(document).ready(function(){
+	$.ajax({
+		url: "/front-end/analytics/data.php",
+		method: "GET",
+		success: function(data) {
+			console.log(data);
+			var item = [];
+			var would = [];
+			
+			//
+        	var borderColors = [];
+        	var backgroundColors = [];
+        	
+        	for (var i=0; i<color.length; i++) {
+        		backgroundColors.push("rgba(" + color[i] + ", 0.2)");
+        		borderColors.push("rgba(" +color[i] + ", 1.0)");
+        	}
+            //
+            
+            
+			for(var i in data) {
+				item.push(data[i].name);
+				would.push(data[i].wouldPurchase);
+			}
+			var chartdata = {
+				labels: item,
+				datasets : [
+					{
+						label: '# of Votes',
+        				backgroundColor: backgroundColors,
+        				borderColor: borderColors,
+        				borderWidth: 1,
+						data: would
+					}
+				]
+			};
 
-function addPieChart(targetID, inputLabels, inputData, backgroundColorsIn) {
-	var ctx = document.getElementById(targetID).getContext('2d');
-	var borderColors = [];
-	var backgroundColors = [];
-	
-	for (var i=0; i<backgroundColorsIn.length; i++) {
-		backgroundColors.push("rgba(" + backgroundColorsIn[i] + ", 0.2)");
-		borderColors.push("rgba(" + backgroundColorsIn[i] + ", 1.0)");
-	}
+			var ctx = $("#pieChart1");
 
-	var pieChart = new Chart(ctx, {
-		type: "pie",
-		data: {
-			labels: inputLabels,
-			datasets: [{
-				label: '# of Votes',
-				data: inputData,
-				backgroundColor: backgroundColors,
-				borderColor: borderColors,
-				borderWidth: 1
-			}]
+			var barGraph = new Chart(ctx, {
+				type: 'pie',
+				data: chartdata
+			});
+		},
+		error: function(data) {
+			console.log(data);
 		}
 	});
-}
+});
