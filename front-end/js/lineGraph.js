aaeddf7af9f27ac6f4793b6fea0dd7bc3260a14f
Haemin Ryu <happygoals@naver.com>
2018-12-01 01:40:19 +0000

    lineGraph is connected with Product DB

--- front-end/js/lineGraph.js
+++ front-end/js/lineGraph.js
@@ -1,39 +1,48 @@
-var labels = ["Kitkat", "Starbucks coffee", "Cute cookie", "Sandwich", "Pizza"];
-var data = [12, 19, 3, 5, 2];
-var lineColor = '255, 99, 132';
+var lineColor = ['255, 99, 132', '54, 162, 235', '255, 206, 86', '75, 192, 192', '153, 102, 255'];
 
-addLineGraph("lineChart1", labels, data, lineColor);
-
-function addLineGraph(targetID, inputLabels, inputData, backgroundColorsIn) {
-	var ctx = document.getElementById(targetID).getContext('2d');
-	var borderColors = "rgba(" + backgroundColorsIn + ", 1.0)";
-	var backgroundColors = "rgba(" + backgroundColorsIn + ", 0.2)";
-
-	var lineGraph = new Chart(ctx, {
-		type: "line",
-		data: {
-			labels: inputLabels,
-			datasets: [{
-				label: '# of Votes',
-				data: inputData,
-				backgroundColor: backgroundColors,
-				borderColor: borderColors,
-				borderWidth: 1
-			}]
-		},
-		options: {
-			scales: {
-				xAxes: [{
-					ticks: {
-					   autoSkip: false
-					}
-				}],
-				yAxes: [{
-					ticks: {
-						beginAtZero: true
-					}
-				}]
+$(document).ready(function(){
+	$.ajax({
+		url: "/front-end/analytics/data.php",
+		method: "GET",
+		success: function(data) {
+			console.log(data);
+			var item = [];
+			var would = [];
+			
+			var borderColors = [];
+        	var backgroundColors = [];
+        	
+        	for (var i=0; i<lineColor.length; i++) {
+        		backgroundColors.push("rgba(" + lineColor[i] + ", 0.2)");
+        		borderColors.push("rgba(" +lineColor[i] + ", 1.0)");
+        	}
+            //
+            for(var i in data) {
+				item.push(data[i].name);
+				would.push(data[i].wouldPurchase);
 			}
+			var chartdata = {
+				labels: item,
+				datasets : [
+					{
+						label: '# of Votes',
+        				backgroundColor: backgroundColors,
+        				borderColor: borderColors,
+        				borderWidth: 1,
+						data: would
+					}
+				]
+			};
+            
+            var ctx = $("#lineChart1");
+
+			var lineGraph = new Chart(ctx, {
+				type: 'line',
+				data: chartdata
+			});
+		},
+		error: function(data) {
+			console.log(data);
 		}
 	});
-}
+});
