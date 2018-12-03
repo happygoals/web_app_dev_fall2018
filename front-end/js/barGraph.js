34edea507f1b464d0ee5371dea8ba8d6d8cff92f
Haemin Ryu <happygoals@naver.com>
2018-12-03 02:31:05 +0000

    put the hoverColor on barGraph

--- front-end/js/barGraph.js
+++ front-end/js/barGraph.js
@@ -37,6 +37,7 @@ $(document).ready(function() {
 						label: '# of Votes',
         				backgroundColor: backgroundColors,
         				borderColor: borderColors,
+        				hoverBackgroundColor: borderColors,
         				borderWidth: 1,
 						data: would
 					}
