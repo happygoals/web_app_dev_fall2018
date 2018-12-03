f875c8c27d7070f6506ea603aadffc35376d83de
Haemin Ryu <happygoals@naver.com>
2018-12-03 01:25:18 +0000

    Fixed the scatterPlot.js data problem. The scatterPolt chart is well-connected with Product database.

--- front-end/js/scatterPlot.js
+++ front-end/js/scatterPlot.js
@@ -1,17 +1,18 @@
 var color = ['255, 99, 132', '54, 162, 235', '255, 206, 86', '75, 192, 192', '153, 102, 255', '255, 159, 64'];
 
-
+	var ctx = $("#scatterPlot1");
+	
 $(document).ready(function(){
 	$.ajax({
 		url: "/front-end/analytics/data.php",
 		method: "GET",
 		success: function(data) {
 			console.log(data);
-			var item = [];
-			var would = [];
+		var item = [];
+		var xPoints = [];
+        var yPoints = [];
+        var would = [];
 
-            var borderColors = [];
-        	var backgroundColors = [];
 
 	//color
 	var colors = [];
@@ -23,49 +24,36 @@ $(document).ready(function(){
         //put the data in the item array and the would array 
     	for(var i in data) {
 				item.push(data[i].name);
-				would.push("{x:"+data[i].wouldPurchase+"," + "y:"+data[i].wouldRemove+"}");   // the value pair of x and y
-			}
-			//chart data
-			var chartdata = {
-				labels: item,
-				datasets : [
-					{
-						label: '# of Votes',
-        				backgroundColor: backgroundColors,
-        				borderColor: borderColors,
-        				borderWidth: 1,
-						data: would
+				//would.push("{x:"+data[i].wouldPurchase+", " + "y:"+data[i].wouldRemove+"}");   // the value pair of x and y
 
-					}
-				]
-			};
-			//option 
-				    var options = {
-				        responsive: true,
-				        title: {
-				            display: true,
-				            position: "top",
-				            text: "Item Popularity (x = 'like', y = 'dislike')",
-				            fontSize: 18,
-				            fontColor: "#111"
-				        },
-				        legend: {
-				            display: true,
-				            position: "bottom",
-				            labels: {
-				                fontColor: "#333",
-				                fontSize: 16
-				        }
-    					 }
-					   };
-
-			var ctx = $("#scatterPlot1");
-
-			var scatterPlot = new Chart(ctx, {
-				type: 'scatter',
-				data: chartdata,
-				options: options
+            xPoints[i] = data[i].wouldPurchase;
+            yPoints[i] = data[i].wouldRemove;
+            x = xPoints[i];
+            y = yPoints[i];
+            var json = {x: x, y: y};
+            would.push(json); 			
+    		
+    	}
+			
+			var scatterChart = new Chart(ctx, {
+			    type: 'scatter',
+			    data: {
+			        datasets: [{
+			            label: 'Scatter Dataset',
+			            data: would,
+			            pointBackgroundColor: colors
+			        }]
+			    },
+			    options: {
+			        scales: {
+			            xAxes: [{
+			                type: 'linear',
+			                position: 'bottom'
+			            }]
+			        }
+			    }
 			});
+
 		},
 		error: function(data) {
 			console.log(data);
